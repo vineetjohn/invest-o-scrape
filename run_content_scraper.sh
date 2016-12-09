6be02401f9629e7c0c399de3957345f478636b3a
Vineet John <vineet.john@uwaterloo.ca>
2016-12-09 10:51:18 -0500

    Minor filepath change in scrape helper

--- run_content_scraper.sh
+++ run_content_scraper.sh
@@ -2,5 +2,5 @@
 
 /usr/bin/python3.5 /home/v2john/PycharmProjects/invest-o-scrape/run.py \
 --mode content-scrape \
---term_list_file_path /home/v2john/Dropbox/Personal/Academic/Masters/UWaterloo/Academics/ResearchProject/Veriday/investopedia/terms_small.txt \
+--term_list_file_path /home/v2john/Dropbox/Personal/Academic/Masters/UWaterloo/Academics/ResearchProject/Veriday/investopedia/terms.txt \
 --output_file_path /home/v2john/scraped_terms.txt
